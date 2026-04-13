1b6755565edf7aa9cecbe6c7bb560b5582247eb4
Hakan <86249094+HDemir23@users.noreply.github.com>
2026-04-13 23:59:42 +0300

    Add: evaluate-reverse-polish-notation - submission-1

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-1.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {string[]} tokens
+     * @return {number}
+     */
+    evalRPN(tokens) {
+        let arr = []
+
+        let ops ={
+            "+" : (a,b) => a+b,
+            "-" : (a,b) => a-b,
+            "*" : (a,b) => a*b,
+            "/" : (a,b) => Math.trunc(a /b)
+        }
+
+        for(let i of tokens){
+            if(ops[i]){
+
+                let b = arr.pop()
+                let a = arr.pop()
+
+                let res = ops[i](a,b)
+
+                arr.push(res)
+            }else{
+                arr.push(Number(i))
+            }
+        }
+        return arr[0]
+    }
+}
